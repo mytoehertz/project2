86291db5450f136208b7cf4b54d5f48124437d35
michael vaysman <mvaysman@gmail.com>
2019-04-25 11:57:27 -0400

    initial commit

---
+++ models/dbContext.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_typescript_1 = require("sequelize-typescript");
+const Student_1 = require("./Student");
+class options {
+}
+let dbConnectionOptions = new options();
+class DbContext {
+    constructor() {
+        this.Context = new sequelize_typescript_1.Sequelize({
+            database: 'burgers_db',
+            dialect: 'mysql',
+            username: 'root',
+            password: '-----'
+            //storage: ':memory:',
+            //modelPaths: [__dirname + '/models']
+        });
+        this.Context.addModels([Student_1.Student]);
+    }
+}
+exports.DbContext = DbContext;
+exports.default = DbContext;
+//# sourceMappingURL=dbContext.js.map
---
+++ models/dbContext.ts
@@ -0,0 +1,30 @@
+import {Sequelize, SequelizeOptions} from "sequelize-typescript";
+import {Student} from "./Student";
+
+
+class options implements SequelizeOptions {
+
+}
+
+let dbConnectionOptions = new options();
+
+
+export class DbContext {
+    Context: Sequelize;
+
+    constructor() {
+        this.Context = new Sequelize({
+            database: 'burgers_db',
+            dialect: 'mysql',
+            username: 'root',
+            password: '-----'
+            //storage: ':memory:',
+            //modelPaths: [__dirname + '/models']
+        });
+        this.Context.addModels([Student]);
+
+    }
+}
+
+
+export default DbContext;
