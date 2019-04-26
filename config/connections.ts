ca6fe6ccf3a3d92fe6322956e9fcfdc39979d727
Mercer Cook <mercercook@dyn-160-39-205-193.dyn.columbia.edu>
2019-04-25 22:29:56 -0400

    Added starter sequelize code with test data

--- config/connections.ts
+++ config/connections.ts
@@ -19,55 +19,113 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-// const Burger = sequelize.define(
-//   "burgers",
-//   {
-//     // attributes
-//     burger_name: {
-//       type: Sequelize.STRING,
-//       allowNull: false
-//     },
-//     description: {
-//       type: Sequelize.STRING
-//       // allowNull defaults to true
-//     },
-//     devoured: {
-//       type: Sequelize.BOOLEAN
-//       // allowNull defaults to true
-//     }
-//   },
-//   {
-//     // options
-//   }
-// );
+const Student = sequelize.define(
+  "students",
+  {
+    // attributes
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    user_email: {
+      type: Sequelize.STRING
+      // allowNull defaults to true
+    },
+    has_started_conversation: {
+      type: Sequelize.BOOLEAN
+      // allowNull defaults to true
+    }
+  },
+  {
+    // options
+  }
+);
 
-// Burger.sync({ force: true }).then(function(thing) {
-//   //   console.log("We managed to sync");
+const Counselor = sequelize.define(
+  "counselors",
+  {
+    // attributes
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    user_email: {
+      type: Sequelize.STRING
+      // allowNull defaults to true
+    },
+    has_replied_to_conversation: {
+      type: Sequelize.BOOLEAN
+      // allowNull defaults to true
+    }
+  },
+  {
+    // options
+  }
+);
 
-//   Burger.create({
-//     burger_name: "Vegetarian",
-//     description: "Product of Michael and Carla",
-//     devoured: false
-//   }).then(function(element) {
-//     // console.log("The added row is: " + element);
-//   });
+const Conversation = sequelize.define(
+  "conversations",
+  {
+    // attributes
+    student_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    counselor_name: {
+      type: Sequelize.STRING
+      // allowNull defaults to true
+    },
+    is_open: {
+      type: Sequelize.BOOLEAN
+      // allowNull defaults to true
+    }
+  },
+  {
+    // options
+  }
+);
 
-//   Burger.create({
-//     burger_name: "Classic",
-//     description: "A burger like Grams used to make",
-//     devoured: false
-//   }).then(function(element) {
-//     // console.log("The added row is: " + element);
-//   });
+//create a test student
 
-//   Burger.create({
-//     burger_name: "Baconator",
-//     description: "The classic with bacon. Lots of bacon.",
-//     devoured: false
-//   }).then(function(element) {
-//     // console.log("The added row is: " + element);
-//   });
-// });
+Student.sync({ force: true }).then(function(thing) {
+  console.log("We managed to sync Students");
+
+  Student.create({
+    username: "Michael",
+    user_email: "Michael@here",
+    has_started_conversation: false
+  }).then(function(element) {
+    console.log("Created a student");
+  });
+});
+
+//creating a test counselor
+
+Counselor.sync({ force: true }).then(function(thing) {
+  console.log("We managed to sync with Counselors");
+
+  Counselor.create({
+    username: "Juan",
+    user_email: "Juan@here",
+    has_replied_to_conversation: true
+  }).then(function(element) {
+    console.log("Created a counselor");
+  });
+});
+
+//creating a test conversation
+
+Conversation.sync({ force: true }).then(function(thing) {
+  console.log("We managed to sync with Conversations");
+
+  Conversation.create({
+    student_name: "Michael",
+    counselor_name: "Juan",
+    is_open: false
+  }).then(function(element) {
+    console.log("Created a conversation");
+  });
+});
 
 // module.exports = {
 //   Sequelize: Sequelize,
