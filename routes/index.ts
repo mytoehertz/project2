99196d753d935013f7fa9bc4e6fd52c1255e54eb
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2019-04-30 16:16:37 -0400

    Added if statement to index routes

--- routes/index.ts
+++ routes/index.ts
@@ -5,6 +5,13 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   console.log("Registering the get request");
+  var randomNumber = Math.random() * 2;
+
+  console.log(randomNumber);
+
+  if (randomNumber > 1) {
+    res.render("signup", { title: "Express" });
+  }
 
   res.render("index", { title: "Express" });
   // res.sendFile(path.join(__dirname, "../public", "category.html"));
