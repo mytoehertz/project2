ff7bb39e4f310f41329480b8131956dc674cf532
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2019-04-30 15:54:09 -0400

    updated index page

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,9 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   console.log("Registering the get request");
-  res.sendFile(path.join(__dirname, "../public", "category.html"));
+
+  res.render("index", { title: "Express" });
+  // res.sendFile(path.join(__dirname, "../public", "category.html"));
   //
 });
 
